d5a4a8f594dbb8d02a9a49ebf735e21ef3ac3b11
manzur349 <dearmanzur@gmail.com>
2025-02-23 14:42:59 -0500

    Update Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,1 +1,20 @@
+pragma solidity ^0.8.20;
 
+import "forge-std/Script.sol";
+import "../src/MyToken.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        // Retrieve the private key from environment variable
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // Start broadcasting transactions
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the contract
+        MyToken token = new MyToken(vm.addr(deployerPrivateKey));
+
+        // Stop broadcasting transactions
+        vm.stopBroadcast();
+    }
+}
